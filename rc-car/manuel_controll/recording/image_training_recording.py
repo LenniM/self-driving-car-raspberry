5664d30504e534c5c9d85cce5c88af139cfa5ad7
LenniM <68513575+LenniM@users.noreply.github.com>
2021-05-22 20:46:24 +0200

    Update image_training_recording.py

--- rc-car/manuel_controll/recording/image_training_recording.py
+++ rc-car/manuel_controll/recording/image_training_recording.py
@@ -26,16 +26,31 @@ class Record_Data_Linux(object):
     def record(self):
         servo_data = self.current_servo_data
         
-        with self.camera as camera:
-            with picamera.array.PiRGBArray(camera) as output:
-                camera.resolution = (1280, 720)
-                camera.capture(output, 'png')
-                image = output.rawCapture.array
-                img_rotate_180 = cv2.rotate(image, cv2.ROTATE_180)
-                cv2.imwrite(os.getcwd() + "/training-data-one", "%s_%03d_%03d.jpg" % ("training-data-one",index,servo_data), img_rotate_180)
-                cv2.waitKey(0)
-                index += 1
-                stream.truncate(0)  
+        with picamera.PiCamera() as camera:
+    # Set the camera's resolution to VGA @40fps and give it a couple
+    # of seconds to measure exposure etc.
+            camera.resolution = (1280, 720)
+            camera.framerate = 80
+            time.sleep(2)
+            # Set up 40 in-memory streams
+            outputs = [io.BytesIO() for i in range(40)]
+            start = time.time()
+            image = camera.capture_sequence(outputs, 'jpeg', use_video_port=True)
+            img_rotate_180 = cv2.rotate(image, cv2.ROTATE_180)
+            cv2.imwrite(os.getcwd() + "/training-data-one", "%s_%03d_%03d.jpg" % ("training-data-one",index,servo_data), img_rotate_180)
+            cv2.waitKey(0)
+            finish = time.time()
+        
+      # with self.camera as camera:
+        #    with picamera.array.PiRGBArray(camera) as output:
+         #       camera.resolution = (1280, 720)
+           #     camera.capture(output, 'png')
+          #      image = output.rawCapture.array
+            #    img_rotate_180 = cv2.rotate(image, cv2.ROTATE_180)
+             #   cv2.imwrite(os.getcwd() + "/training-data-one", "%s_%03d_%03d.jpg" % ("training-data-one",index,servo_data), img_rotate_180)
+              #  cv2.waitKey(0)
+               # index += 1
+                #stream.truncate(0)  
         #with self.camera as camera:
        #     stream = io.BytesIO()
       #      for x in camera.capture_continuous(stream, format="png"):
