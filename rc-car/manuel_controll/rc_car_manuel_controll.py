b01929f41bcfabe6603b34a43253938d01a575af
LenniM <68513575+LenniM@users.noreply.github.com>
2021-06-02 16:43:45 +0200

    Update rc_car_manuel_controll.py

--- rc-car/manuel_controll/rc_car_manuel_controll.py
+++ rc-car/manuel_controll/rc_car_manuel_controll.py
@@ -317,7 +317,8 @@ class MovementControllerLinux:
 
                 print("a clicked")
             elif(val == 114):
-                Record_Data_Linux(self.initial_servo_speed).startRecording()
+		startRecordingThread = threading.Thread(thread=Record_Data_Linux(self.initial_servo_speed).startRecording())
+                startRecordingThread.start()
                 print("r clicked")
 
             
